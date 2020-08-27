523707188590074472022a98982ae1a136c6764b
GMoraesGarcia <g.moraes.garcia@gmail.com>
2020-08-27 10:57:22 -0300

    Tabuada

--- exemplo-git/src/main/java/com/mycompany/exemplogit/Principal.java
+++ exemplo-git/src/main/java/com/mycompany/exemplogit/Principal.java
@@ -5,12 +5,26 @@
  */
 package com.mycompany.exemplogit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Gabriel
  */
 public class Principal {
+    public static void mostrarTabuado(int num){
+        for(int i =0;i<11;i++)
+            System.out.println("" + num + " X " + i+ "=" + (i*num));
+    
+    }
+    
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("tabuada do :");
+        int num = sc.nextInt();
+        
+        mostrarTabuado(num);
         System.out.println("teste");
         System.out.println("teste2");
     }
